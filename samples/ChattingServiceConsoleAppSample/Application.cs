70b16f0b086b0a500a33c93c7125581e3957e9e4
N1K0232 <ns.nicolasilvestri@libero.it>
2024-12-10 22:33:53 +0100

    Add samples project

---
+++ samples/ChattingServiceConsoleAppSample/Application.cs
@@ -0,0 +1,44 @@
+﻿using ChattingService;
+using ChattingService.Models;
+
+namespace ChattingServiceConsoleAppSample;
+
+public class Application
+{
+    public async Task ExecuteAsync()
+    {
+        Console.WriteLine("Connecting . . .");
+
+        using var primaryClient = new ChatClient();
+        await primaryClient.ConnectAsync("127.0.0.1", 5000);
+
+        using var secondaryClient = new ChatClient();
+        await secondaryClient.ConnectAsync("127.0.0.1", 5000);
+
+        Console.Write("Type your message: ");
+        string? text = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return;
+        }
+
+        var message = new ChatMessage
+        {
+            Text = text,
+            SenderId = Guid.NewGuid(),
+            RecipientId = Guid.NewGuid()
+        };
+
+        await primaryClient.SendAsync(message);
+        await secondaryClient.ReceiveAsync(SecondaryClientReceivedAsync);
+    }
+
+    private async Task SecondaryClientReceivedAsync(ChatMessage message, CancellationToken cancellationToken)
+    {
+        await Task.Delay(500, cancellationToken);
+        string responseMessage = string.Format("Message received: {0}", message.Text);
+
+        Console.WriteLine(responseMessage);
+    }
+}
---
+++ samples/ChattingServiceConsoleAppSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using ChattingService;
+using ChattingServiceConsoleAppSample;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(ConfigureServices)
+    .Build();
+
+var application = host.Services.GetRequiredService<Application>();
+await host.RunAsync();
+await application.ExecuteAsync();
+
+void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+{
+    var settings = new ChatSettings
+    {
+        Port = 5000,
+        MaxClientsCount = 10
+    };
+
+    services.AddSingleton(settings);
+
+    services.AddSingleton<IChatServer, ChatServer>();
+    services.AddSingleton<IChatClient, ChatClient>();
+
+    services.AddSingleton<Application>();
+    services.AddHostedService<ChatServerBackgroundService>();
+}
